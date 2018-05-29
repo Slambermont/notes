b78f39d40aa42b3e62c67f4c5ace9fadbe3d52bf
Charlene <charlenechetcuti@gmail.com>
2018-05-29 17:36:55 +0100

    add it block functionality

--- assert.js
+++ assert.js
@@ -1,11 +1,21 @@
-var expect = function (value1) {
-	return {
+function expect (value1) {
+
+ return {
 		toEqual: function (value2) {
 			if (value1 === value2) {
-        console.log('%cTest passed!', 'color: green')
+				console.log('%cTest passed!', 'color: green')
       } else {
         console.log(`%cTest failed: expected ${value1} to equal ${value2}.`, 'color: red')
       }
+
 		}
 	}
+
+}
+
+function it (description, callBack) {
+
+	console.log(description)
+	callBack()
+
 }
