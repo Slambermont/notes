091336cae27d7cb4b1e1014d6a430cde10b5500a
Salome Lambermont <salome.lambermont@gmail.com>
2018-05-29 17:18:13 +0100

    Display each note in its own div

--- noteRender.js
+++ noteRender.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var notes = "Note1, Note2";
+  var notes = ['Note1', 'Note2'];
 
-  document.getElementById("notes").innerHTML = notes;
+  var noteContainer = document.getElementById("notes")
 
-
+  notes.forEach(function(note){
+    var newDiv = document.createElement('div')
+    var text = document.createTextNode(note);
+    newDiv.appendChild(text);
+    noteContainer.appendChild(newDiv);
+  });
 });
