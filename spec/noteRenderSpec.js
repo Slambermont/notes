49bd8a6506c9a1de871cbaa9566382e9375b09da
Salome Lambermont <salome.lambermont@gmail.com>
2018-05-31 14:29:40 +0100

    Feature test - display notes

--- spec/noteRenderSpec.js
+++ spec/noteRenderSpec.js
@@ -1,16 +1,4 @@
-describe('notes', function() {
-  it('equals', function() {
-    expect(1).toEqual(1)
-  })
-  it('does not equal', function() {
-    expect(1).toEqual(2)
-  })
-})
-describe('arrays', function() {
-  it('can do arrays', function() {
-    expect([1]).toEqual([1])
-  })
-  it('errors arrays', function() {
-    expect([1]).toEqual([2])
-  })
-})
+document.addEventListener("DOMContentLoaded", function() {
+  expect(document.documentElement).toHaveContent('Note1')
+  expect(document.documentElement).toHaveContent('Note2')
+});
