5ea9d2b8d5e2048feaafe47ead7f907714b12586
Salome Lambermont <salome.lambermont@gmail.com>
2018-05-31 14:47:59 +0100

    Add ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": "airbnb-base"
 };
