5fcc61effa51a574b37f0b6c04fabfaca73b433f
Salome Lambermont <salome.lambermont@gmail.com>
2018-05-31 15:08:21 +0100

    Create note object

--- models/noteRender.js
+++ models/noteRender.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var notes = ['Note1', 'Note2'];
+document.addEventListener("DOMContentLoaded", () => {
+  const notes = ['Note1', 'Note2'];
 
-  var noteContainer = document.getElementById("notes")
+  const noteContainer = document.getElementById('notes')
 
-  notes.forEach(function(note){
-    var newDiv = document.createElement('div')
-    var text = document.createTextNode(note);
+  notes.forEach((note) => {
+    const newDiv = document.createElement('div')
+    const text = document.createTextNode(note);
     newDiv.appendChild(text);
     noteContainer.appendChild(newDiv);
   });
--- spec/features/noteRenderSpec.js
+++ spec/features/noteRenderSpec.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   expect(document.documentElement).toHaveContent('Note1')
   expect(document.documentElement).toHaveContent('Note2')
 });
