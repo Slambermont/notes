385950045e8cc2ab606e344ed13a3b8f25c8cec4
Revilo1882 <oliverrharris@gmail.com>
2018-05-31 17:05:18 +0100

    adds a form and create function to the note html page

--- models/noteRender.js
+++ models/noteRender.js
@@ -1,10 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const notes = ['Note1', 'Note2'];
+document.addEventListener('DOMContentLoaded', () => {
+  const noteList = new NoteList();
+  const currentNotes = noteList.notes;
+  const noteContainer = document.getElementById('notes');
+  const button = document.getElementById('submit');
 
-  const noteContainer = document.getElementById('notes')
-
-  notes.forEach((note) => {
-    const newDiv = document.createElement('div')
+  button.onclick = function click() {
+    const text = document.getElementById('noteform').value;
+    noteList.createNote(text);
+  };
+  currentNotes.forEach((note) => {
+    const newDiv = document.createElement('div');
     const text = document.createTextNode(note);
     newDiv.appendChild(text);
     noteContainer.appendChild(newDiv);
--- spec/features/noteRenderSpec.js
+++ spec/features/noteRenderSpec.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  expect(document.documentElement).toHaveContent('Note1')
-  expect(document.documentElement).toHaveContent('Note2')
+  expect(document.documentElement).toHaveContent('Notes')
+  expect(document.documentElement).toHaveContent('Create')
 });
