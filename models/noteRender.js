3751754fceddc8c180536b86a88739cca49a89f0
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-01 11:39:02 +0100

    Create note functionality completed

--- models/noteRender.js
+++ models/noteRender.js
@@ -1,17 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   const noteList = new NoteList();
   const currentNotes = noteList.notes;
-  const noteContainer = document.getElementById('notes');
+  const noteContainer = document.getElementById('noteContainer');
   const button = document.getElementById('submit');
 
-  button.onclick = function click() {
+  function replaceNoteList() {
+    const oldNotes = document.getElementById('notes');
+    const newNotes = document.createElement('div');
+    newNotes.setAttribute('id', 'notes');
+    noteContainer.replaceChild(newNotes, oldNotes);
+  }
+
+  function renderNotes() {
+    const notes = document.getElementById('notes');
+    currentNotes.forEach((note) => {
+      const text = document.createTextNode(note.text);
+      const article = document.createElement('article');
+      article.appendChild(text);
+      notes.appendChild(article);
+    });
+  }
+
+  button.addEventListener('click', (clickEvent) => {
+    clickEvent.preventDefault();
     const text = document.getElementById('noteform').value;
     noteList.createNote(text);
-  };
-  currentNotes.forEach((note) => {
-    const newDiv = document.createElement('div');
-    const text = document.createTextNode(note);
-    newDiv.appendChild(text);
-    noteContainer.appendChild(newDiv);
+    replaceNoteList();
+    renderNotes();
   });
 });
